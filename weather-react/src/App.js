8685819a3b144bcff8b038e75f3d80648686079d
Jan <janwajdenfeld@gmail.com>
2021-07-07 04:52:40 -0500

    commit

--- weather-react/src/App.js
+++ weather-react/src/App.js
@@ -1,95 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import Search from './api/Search'
+import Display from './components/display'
 
-const api = {
-    key: process.env.REACT_APP_API_KEY,
-    base: "//api.openweathermap.org/data/2.5/",
-    jsonkey: process.env.REACT_APP_JSON_KEY
-}
 
-var options = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-  let date = new Date();
-  date = date.toLocaleDateString("en-US", options);
+//<Display weatherData = {weatherData} loading = {loading}/>
+
 
 function App(){
-    const[query, setQuery] = useState('');
-    const[weather, setWeather] = useState({});
-
-    const search = evt => {
-        if (evt.key === "Enter") {
-          fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
-            .then(res => res.json())
-            .then(result => {
-              setWeather(result);
-              setQuery('');
-              console.log(result);
-            });
-        }
-      }
-
-    const config = {
-    headers: {
-        "X-Master-Key": api.jsonkey
-    }
-    };
-    const postInfo = async() => {
-        const res = await axios.post('https://api.jsonbin.io/v3/b/', 
-        {
-            data: {
-                number: 42
-            },
-        },
-        { headers: { ...config.headers, "X-Bin-Private": false } })
-        console.log(res);
-    }
-    
-    const fetchInfo = async() => {
-        const res = await axios.get('https://api.jsonbin.io/v3/b/60e11917fe016b59dd5b7183', {
-            headers: { ...config.headers, "X-Bin-Meta": false } })
-
-        console.log(res.data);
-    }
-    useEffect(() => {
-        postInfo();
-        fetchInfo();
-    },);
+    const[weatherData, setWeatherData] = useState(null);
+    const[loading, setLoading] = useState();
 
     return(
         <div className="app">
             <main>
-                <div className="search-box">
-                    <input 
-                        type="text"
-                        className="search-bar"
-                        placeholder="Search..."
-                        onChange={e => setQuery(e.target.value)}
-                        value={query}
-                        onKeyPress={search}
-                    />
-                </div>
-                {(typeof weather.main != "undefined") ? (
-
-                <div className="info">
-                    <div className="info-box">
-
-                        <div className="location">{weather.name}, {weather.sys.country}</div>
-                        <div className="date">{date}</div>
-                    </div>
-
-                    <div className="weather-box">
-                        <div className="temp">
-                            {weather.main.temp}°F
-                        </div>
-                        <div className="weather">{weather.weather[0].main}</div>
-                    </div>
-                </div>
-                ) : ('')}
-
+                <Search setWeatherData = {setWeatherData} setLoading = {setLoading}/>
+                <Display weatherData = {weatherData} loading = {loading}/>
                 
             </main>
         </div>
--- weather-react/src/backup.js
+++ weather-react/src/backup.js
@@ -1,67 +1,76 @@
-import React, { useState } from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
+
 const api = {
-  key: "7d4ef5d23648c7c62acd3ccfa1a0a30b",
-  base: "https://api.openweathermap.org/data/2.5/"
+    key: process.env.REACT_APP_API_KEY,
+    base: "//api.openweathermap.org/data/2.5/",
+    jsonkey: process.env.REACT_APP_JSON_KEY
 }
 
-function App() {
-  const [query, setQuery] = useState('');
-  const [weather, setWeather] = useState({});
+var options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  let date = new Date();
+  date = date.toLocaleDateString("en-US", options);
 
-  const search = evt => {
-    if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => {
-          setWeather(result);
-          setQuery('');
-          console.log(result);
-        });
-    }
-  }
+function App(){
+    const[query, setQuery] = useState('');
+    const[weather, setWeather] = useState({});
+    const[loading, setLoading] = useState(false);
+    const search = evt => {
+        if (evt.key === "Enter") {
+            setLoading(true);
+            fetch(`${api.base}weather?q=${query}&units=imperial&APPID=${api.key}`)
+            .then(res => res.json())
+            .then(result => {
+                setWeather(result);
+                setQuery('');
+                console.log(result);
+                setLoading(false);
+            });
+        }
+      }
 
-  const dateBuilder = (d) => {
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    return(
+        <div className="app">
+            <main>
+                <div className="search-box">
+                    <input 
+                        type="text"
+                        className="search-bar"
+                        placeholder="Search..."
+                        onChange={e => setQuery(e.target.value)}
+                        value={query}
+                        onKeyPress={search}
+                    />
+                </div>
+                {(typeof weather.main != "undefined") ? (
 
-    return `${day} ${date} ${month} ${year}`
-  }
+                <div className="info">
+                    <div className="info-box">
 
-  return (
-    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
-      <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
+                        <div className="location">{weather.name}, {weather.sys.country}</div>
+                        <div className="date">{date}</div>
+                    </div>
+
+                    <div className="weather-box">
+                        <div className="temp">
+                            {weather.main.temp}°F
+                        </div>
+                        <div className="weather">{weather.weather[0].main}</div>
+                    </div>
+                </div>
+                ) : ('')}
+
+                
+            </main>
         </div>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
-            </div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
-        </div>
-        ) : ('')}
-      </main>
-    </div>
-  );
+    )
+
 }
 
 export default App;
+
