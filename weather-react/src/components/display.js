79def4f2ec25ef6493ca86a09227e091e162cb4f
Jan <janwajdenfeld@gmail.com>
2021-07-07 08:44:35 -0500

    commit

--- weather-react/src/components/display.js
+++ weather-react/src/components/display.js
@@ -14,15 +14,15 @@ const Display = (props) => {
             <div className="info">
                 <div className="info-box">
 
-                    <div className="location">{props.name}, {props.sys.country}</div>
+                    <div className="location">{props.weatherData.name}, {props.weatherData.sys.country}</div>
                     <div className="date">{date}</div>
                 </div>
 
                 <div className="weather-box">
                     <div className="temp">
-                        {props.main.temp}°F
+                        {props.weatherData.main.temp}°F
                     </div>
-                    <div className="weather">{props.weather[0].main}</div>
+                    <div className="weather">{props.weatherData.weather[0].main}</div>
                 </div>
             </div>
         )
